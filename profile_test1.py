c2653db883f2002f8ddfa1f39f42e285ac30263c
Eric Chlebek <echlebek@gmail.com>
2015-04-21 23:41:40 -0700

    Add profile tests

---
+++ profile_test1.py
@@ -0,0 +1,12 @@
+import numpy as np
+from fifteen import BLANK
+from fifteen import solve
+
+if __name__ == "__main__":
+    board = np.array([[14, 9, 5, 8],
+                      [1, 15, 4, 2],
+                      [6, BLANK, 3, 11],
+                      [10, 7, 13, 12]],
+                     dtype=np.int8)
+    for _ in range(10):
+        solve(board)
